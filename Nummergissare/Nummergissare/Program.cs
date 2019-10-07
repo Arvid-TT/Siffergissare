7c5ba8c833d9455ca41db514e31af2dfeb12ca64
Arvid Toft <arto02002@utb.vaxjo.se>
2019-10-07 20:01:45 +0200

    Koden

---
+++ Nummergissare/Nummergissare/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Nummergissare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gissa ett tal mellan 1 och 100");
+            Random slump = new Random();
+            int tal = slump.Next(100);
+            int antal = 1;
+            int gissning;
+            while (true)
+            {
+                gissning = int.Parse(Console.ReadLine());
+                if (tal == gissning)
+                {
+                    Console.WriteLine("Grattis, du gissade rätt. Talet var " + tal + ".");
+                    Console.WriteLine("Det to dig " + antal + " försök att gissa rätt.");
+                    break;
+                }
+                else if (tal > gissning)
+                {
+                    Console.WriteLine("Du gissade för lågt, försök igen!");
+                }
+                else if (tal < gissning)
+                {
+                    Console.WriteLine("Du gissade för högt, försök igen!");
+                }
+                antal++;
+            }
+        }
+    }
+}
